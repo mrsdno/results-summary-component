3872d116e2687df8a460e4e782a75f24bc1f124f
mrsdno <christine@dno.dev>
2023-07-05 22:45:00 -0400

    set up components and start some initial styling

---
+++ src/components/Summary.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Summary.vue',
+  data() {}
+})
+</script>
+
+<template>
+  <div class="summary-wrapper">
+    <div class="heading">Summary</div>
+    <div class="score-section">
+      <div class="sub-heading">Reaction</div>
+      <div class="score-detail"><span>80</span>/ 100</div>
+    </div>
+    <div class="score-section">
+      <div class="sub-heading">Memory</div>
+      <div class="score-detail"><span>92</span>/ 100</div>
+    </div>
+    <div class="score-section">
+      <div class="sub-heading">Verbal</div>
+      <div class="score-detail"><span>61</span>/ 100</div>
+    </div>
+    <div class="score-section">
+      <div class="sub-heading">Visual</div>
+      <div class="score-detail"><span>72</span>/ 100</div>
+    </div>
+    <button>Continue</button>
+  </div>
+</template>
+
+<style lang="scss">
+.summary-wrapper {
+  background-color: cyan;
+  width: 250px;
+  height: 350px;
+}
+</style>
---
+++ src/components/YourResult.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'YourResult.vue',
+  data() {}
+})
+</script>
+
+<template>
+  <div class="result-wrapper">
+    <div class="heading">Your Result</div>
+    <div class="circle">
+      <div class="number-score">76</div>
+      of 100
+    </div>
+    <div class="word-score">Great</div>
+    <div class="score-description">
+      You scored higher than 65% of the people who have taken these tests.
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.result-wrapper {
+  background-color: purple;
+  width: 250px;
+  height: 350px;
+}
+</style>
